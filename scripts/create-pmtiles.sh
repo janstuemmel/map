61fa7d23695a04363d40f39b512f09146722f7f1
Jan S. <7893682+janstuemmel@users.noreply.github.com>
2026-09-26 15:55:15 +0200

    cleanup scripts

--- scripts/create-pmtiles.sh
+++ scripts/create-pmtiles.sh
@@ -1,70 +1,54 @@
 #!/bin/bash
+set -e
 
 out=output
 osmpbf=$out/osm.pbf/alg-ly-tn-combined.osm.pbf
 osmfiltered=$out/osm.pbf/filtered
 geojsons=$out/osm.pbf/geojsons
+mkdir -p $osmfiltered
+mkdir -p $geojsons
 
 bbox_africa=-14.37,10.433,36.507,48.42
 bbox_roads=0.831,24.531,17.904,37.429 # from eat algeria to west libya
 
-# mkdir -p $osmfiltered && rm -rf $osmfiltered/* &&
-# mkdir -p $geojsons && rm -rf $geojsons/* &&
-
-# # FILTER
-
+# # boundaries
+# echo; echo Build boundaries
 # osmium tags-filter $osmpbf a/boundary=administrative --overwrite -o $osmfiltered/boundaries.osm.pbf &&
-# osmium tags-filter $osmpbf w/natural=water wr/waterway=river,stream --overwrite -o $osmfiltered/water.osm.pbf &&
-
-# # places
-# osmium extract --bbox=$bbox_roads $osmpbf --overwrite -o $osmfiltered/places-tmp.osm.pbf &&
-# osmium tags-filter $osmfiltered/places-tmp.osm.pbf n/place=city,town,village n/capital --overwrite -o $osmfiltered/places.osm.pbf
-# rm -f $osmfiltered/places-tmp.osm.pbf
+# osmium export $osmfiltered/boundaries.osm.pbf --overwrite -o $geojsons/boundaries.geojson &&
 
 # # roads
-# osmium extract --bbox=$bbox_roads $osmpbf --overwrite -o $osmfiltered/roads-tmp.osm.pbf &&
-# osmium tags-filter $osmfiltered/roads-tmp.osm.pbf w/highway --overwrite -o $osmfiltered/roads.osm.pbf &&
-# rm -f $osmfiltered/roads-tmp.osm.pbf
+# echo; echo Build roads
+# osmium extract --bbox=$bbox_roads $osmpbf --overwrite -o $osmfiltered/roads-tmp.osm.tmp.pbf &&
+# osmium tags-filter $osmfiltered/roads-tmp.osm.tmp.pbf w/highway=track,path,bridleway,unclassified,motorway,trunk,primary,secondary,tertiary --overwrite -o $osmfiltered/roads.osm.pbf &&
+# osmium export $osmfiltered/roads.osm.pbf --overwrite -o $geojsons/roads.tmp.geojson &&
+# rm -f $osmfiltered/roads-tmp.osm.tmp.pbf &&
+# ogr2ogr -simplify 0.001 $geojsons/roads.geojson $geojsons/roads.tmp.geojson
+# rm -f $geojsons/roads.tmp.geojson &&
 
-# # to GEOJSON
-# osmium export $osmfiltered/boundaries.osm.pbf -o $geojsons/boundaries.geojson &&
-# osmium export $osmfiltered/places.osm.pbf -o $geojsons/places.geojson &&
-# osmium export $osmfiltered/water.osm.pbf -o $geojsons/water.geojson &&
-# osmium export $osmfiltered/roads.osm.pbf -o $geojsons/roads.geojson &&
+# places
+osmium extract --bbox=$bbox_roads $osmpbf --overwrite -o $osmfiltered/places-tmp.osm.pbf &&
+osmium tags-filter $osmfiltered/places-tmp.osm.pbf n/place=city,town n/capital --overwrite -o $osmfiltered/places.osm.pbf &&
+rm -f $osmfiltered/places-tmp.osm.pbf &&
+osmium export $osmfiltered/places.osm.pbf --overwrite -o $geojsons/places.geojson &&
 
-# # assign explicit minzoom per place so tippecanoe's density-based thinning
-# # doesn't drop capitals/cities in favor of denser village clusters
-# jq '.features[] |= (
-#   if .properties.place == "city" and .properties.capital == "yes" then .properties.tippecanoe = {"minzoom": 0}
-#   elif .properties.place == "city" and (.properties.capital != null) then .properties.tippecanoe = {"minzoom": 3}
-#   elif .properties.place == "city" then .properties.tippecanoe = {"minzoom": 5}
-#   elif .properties.place == "town" then .properties.tippecanoe = {"minzoom": 7}
-#   else .properties.tippecanoe = {"minzoom": 9}
-#   end
-# )' $geojsons/places.geojson > $geojsons/places.geojson.tmp &&
-# mv $geojsons/places.geojson.tmp $geojsons/places.geojson &&
-
-
-CMD=(
-  -Z0 -z12
-  --clip-bounding-box=$bbox_africa
-  -L ocean:$out/ocean/ocean-simple.geojson
-  -L roads:$geojsons/roads.geojson
-  # -L boundaries:$geojsons/boundaries.geojson
-  -L places:$geojsons/places.geojson
-  # -L water:$geojsons/water.geojson
-  -y highway -y name -y admin_level -y boundary -y place -y water -y natural
-  --simplification=10
-  --drop-densest-as-needed
-  --calculate-feature-density
-  --no-feature-limit
-  --no-tile-size-limit
-  --force 
-)
+tippecanoe -Z0 -z10 \
+  --clip-bounding-box=$bbox_africa \
+  -o $out/map.pmtiles \
+  -L ocean:$out/ocean/ocean.geojson \
+  -L roads:$geojsons/roads.geojson \
+  -L boundaries:$geojsons/boundaries.geojson \
+  -L places:$geojsons/places.geojson \
+  -y highway -y name -y name:en -y name:de -y admin_level -y boundary -y place -y water -y natural -y capital \
+  --simplification=10 \
+  --drop-rate=1 \
+  --drop-densest-as-needed \
+  --calculate-feature-density \
+  --no-feature-limit \
+  --no-tile-size-limit \
+  --force &&
+echo
 
 
-tippecanoe -o $out/map.pmtiles "${CMD[@]}" &&
-
-du -h $out/* &&
+du -h $out/*
 
 cp $out/map.pmtiles public/data
--- scripts/get-ocean.sh
+++ scripts/get-ocean.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 dir=output/ocean
-name=ne_10m_ocean
+name=water-polygons-split-3857
+bbox_africa=-14.37,10.433,36.507,48.42
 
 mkdir -p $dir &&
 
-wget -O $dir/$name.zip https://naciscdn.org/naturalearth/10m/physical/$name.zip &&
- 
+[ -f $dir/$name.zip ] || wget -O $dir/$name.zip https://osmdata.openstreetmap.de/download/$name.zip &&
+
 unzip -u $dir/$name.zip -d $dir &&
 
 rm -f $dir/ocean.geojson &&
 
-ogr2ogr -f GeoJSON $dir/ocean-simple.geojson $dir/$name.shp &&
+ogr2ogr -t_srs EPSG:4326 $dir/water_4326.shp $dir/$name/water_polygons.shp &&
 
-jq '.features[].properties.tippecanoe = {"minzoom": 0, "maxzoom": 14}' $dir/ocean-simple.geojson > $dir/ocean-simple.geojson.tmp
-mv $dir/ocean-simple.geojson.tmp $dir/ocean-simple.geojson
+ogr2ogr -f GeoJSON -clipsrc ${bbox_africa//,/ } $dir/ocean.geojson $dir/water_4326.shp &&
+
+jq '.features[].properties.tippecanoe = {"minzoom": 0, "maxzoom": 8}' $dir/ocean.geojson > $dir/ocean.geojson.tmp
+mv $dir/ocean.geojson.tmp $dir/ocean.geojson
 
 du -h $dir/*
